f7275d7914aa383ee014ed9e4a442a045979e377
Brian <musyimib449@gmail.com>
2025-02-28 03:41:44 +0300

    function parameters defult overidden and two parameters

---
+++ index.js
@@ -0,0 +1,21 @@
+function introduction(name){
+    return `Hi, my name is ${name}.`;
+}
+console.log(introduction("aki"))
+
+
+function introductionWithLanguage(name, language){
+   return`Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+    console.log(introductionWithLanguage("Aki", "Ember.js"));
+
+
+function introductionWithLanguageOptional(name, language){
+   return`Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+introductionWithLanguageOptional("Gracie", "JavaScript");
+
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+    return`Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+introductionWithLanguageOptional("Gracie", "JavaScript");
